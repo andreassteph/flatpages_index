6adcc45a76aa38b8fbbe28fb55adad0093515b28
Andreas Stephanides <andreas.stephanides@gmail.com>
2020-02-12 13:51:33 +0100

    fix relative path

--- flatpages_index/__init__.py
+++ flatpages_index/__init__.py
@@ -1,3 +1,3 @@
-from flatpages import FlatPagesIndex
-from flatpages import FileLists
-from utils import LinkElement
+from .flatpages import FlatPagesIndex
+from .flatpages import FileLists
+from .utils import LinkElement
